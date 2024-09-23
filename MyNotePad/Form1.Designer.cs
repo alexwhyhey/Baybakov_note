051f8ae335fd3e655b7e4634d2e8f632315ee598
alexwhyhey <rystambaybakov@gmail.com>
2024-09-23 12:56:54 +0500

    добавили чекбокс и метку, переименовали форму

--- MyNotePad/Form1.Designer.cs
+++ MyNotePad/Form1.Designer.cs
@@ -29,32 +29,58 @@
         private void InitializeComponent()
         {
             button1 = new Button();
+            checkBox1 = new CheckBox();
+            label1 = new Label();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(465, 151);
+            button1.Location = new Point(38, 37);
             button1.Name = "button1";
-            button1.Size = new Size(184, 64);
+            button1.Size = new Size(125, 46);
             button1.TabIndex = 0;
             button1.Text = "Закрыть";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(38, 160);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(101, 24);
+            checkBox1.TabIndex = 1;
+            checkBox1.Text = "checkBox1";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(38, 236);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 2;
+            label1.Text = "label1";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(255, 128, 0);
-            ClientSize = new Size(1088, 383);
+            ClientSize = new Size(321, 327);
+            Controls.Add(label1);
+            Controls.Add(checkBox1);
             Controls.Add(button1);
             Name = "Form1";
-            Text = "Блокнот";
+            Text = "123";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button button1;
+        private CheckBox checkBox1;
+        private Label label1;
     }
 }
