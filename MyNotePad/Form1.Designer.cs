086d2f635b2ac5dafb0e3baf02c87539790c8d5b
alexwhyhey <rystambaybakov@gmail.com>
2024-09-23 12:50:21 +0500

    Revert "зеленая форма и нет кнопки закрыть"
    This reverts commit 3b599bebb13e33c40beabb2bb16a3e4184c26f91.

--- MyNotePad/Form1.Designer.cs
+++ MyNotePad/Form1.Designer.cs
@@ -28,19 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(465, 151);
+            button1.Name = "button1";
+            button1.Size = new Size(184, 64);
+            button1.TabIndex = 0;
+            button1.Text = "Закрыть";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.Lime;
+            BackColor = Color.FromArgb(255, 128, 0);
             ClientSize = new Size(1088, 383);
+            Controls.Add(button1);
             Name = "Form1";
             Text = "Блокнот";
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
     }
 }
